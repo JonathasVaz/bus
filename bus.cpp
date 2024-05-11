741797bdd5d6918cf778177b2ee26ae6049b201b
Jonathas Vaz <jonathasvazdesouza@gmail.com>
2024-05-11 15:43:43 -0300

    update

--- bus.cpp
+++ bus.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 
 int corredor [25] = {0};
 int janela [25] = {0};
@@ -39,25 +41,66 @@ void verificarjanela() {
 		}
 	}
 }
+void venderPassagem(int poltrona, int tipo) {
+    if (tipo == 1) {
+        if (corredor[poltrona] == 'X') {
+            printf("\nPoltrona j� vendida.\n");
+        } else {
+            corredor[poltrona] = 'X'; // Coloca 'X' na poltrona vendida
+            printf("\nPassagem vendida com sucesso!\n");
+        }
+    } else {
+        if (janela[poltrona] == 'X') {
+            printf("\nPoltrona j� vendida.\n");
+        } else {
+            janela[poltrona] = 'X'; // Coloca 'X' na poltrona vendida
+            printf("\nPassagem vendida com sucesso!\n");
+        }
+    }
+}
 
 int main(){
-	int poltronas, escolha;
-	printf("Digite (1) para ver poltronas no corredor.\nDigite (2) para ver poltronas na janela.\nDigite (3) para ver todas poltronas. -> ");
-  	 	scanf("%d", &escolha);	
-	switch (escolha){
-		case 1:
-			verificarcorredor();
-			break;	
+	setlocale(LC_ALL, "Portuguese");
 	
- 		case 2:
- 			verificarjanela();
- 			break;
- 		case 3:
- 			verificartudo();
- 			break;
- }
- 	
- 	
- 	
- 	
+	int poltronas, escolha, poltrona;
+    while (1) {
+        printf("\n-------------------------------------------\nDigite (1) para ver poltronas no corredor.");
+        printf("\nDigite (2) para ver poltronas na janela.");
+        printf("\nDigite (3) para ver todas poltronas.");
+        printf("\nDigite (4) para comprar passagem.");
+        printf("\nDigite (0) para sair. ->\n------------------------------------------- ");
+        
+        if (scanf("%d", &escolha) != 1) { 
+            printf("Entrada inv�lida. Por favor, insira um n�mero v�lido.\n");
+            while (getchar() != '\n'); // Limpa o buffer do teclado
+            continue;
+        }
+        
+        switch (escolha) {
+            case 1:
+                verificarcorredor();
+                break;
+            case 2:
+                verificarjanela();
+                break;
+            case 3:
+                verificartudo();
+                break;
+            case 4:
+                printf("Digite o n�mero da poltrona: ");
+                scanf("%d", &poltrona);
+                printf("Digite 1 para corredor ou 2 para janela: ");
+                scanf("%d", &poltronas);
+                venderPassagem(poltrona, poltronas);
+                break;
+            case 0:
+                printf("\nSaindo do programa...\n");
+                return 0;
+            default:
+                printf("\nOp��o inv�lida. Por favor, tente novamente.\n");
+                break;
+        }
+    }
+
+    return 0;
 }
